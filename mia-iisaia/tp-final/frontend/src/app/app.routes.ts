da8585a74df4e14d8af9cab9b17feda0cc7cebab
brunomaso1 <brunomaso1@hotmail.com>
2026-06-15 18:35:37 -0300

    fix: add pathMatch full to portal root route

--- mia-iisaia/tp-final/frontend/src/app/app.routes.ts
+++ mia-iisaia/tp-final/frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
     loadChildren: () => import('./features/portal/portal.routes').then((m) => m.portalRoutes),
   },
 ];
